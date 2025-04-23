86a1e7bd0d32f8e0c32769bd630cb2f012416fd9
M Hamza <lahgum01@gmail.com>
2025-04-23 13:40:48 +0530

    Two Sum Problem

--- practice.php
+++ practice.php
@@ -1,4 +1,21 @@
 <?php
-
-echo("HEllo am  working !");
+   // Two Sum Problem 
+   $target = 10;
+                        /// target = 4 
+                        /// 1,3,4,5,7,7,5  /// 1 ,3   ;  
+   $array  = [1,5,2,6,4,9];
+// 1, 9 , 6,4
+   for($i = 0 ; $i <count($array); $i++)
+   {
+        for($j = $i+1; $j< count($array);$j++)
+        {
+            $chckTarget = $array[$i] + $array[$j];
+            if($chckTarget === $target)
+            {
+                echo "Found at ". $array[$i] ." ". $array[$j];
+                break 2;   
+            }
+        }
+   }
 ?>
+
