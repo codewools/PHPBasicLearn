4e1c7a20b1a91c91a8501d0196a0f38efb7505d5
M Hamza <lahgum01@gmail.com>
2025-04-25 13:01:59 +0530

    HTML code added Click Me button

--- practice.php
+++ practice.php
@@ -1,13 +1,56 @@
 <?php
-   
-   $name =  "Hamza";
-   $age =  "23";
-   $height =  "6.4";
+// Your PHP code (if any) goes here
 
-    echo "<h1>Hi, I'm ".$name.". I'm ".$age." years old and ".$height." feet tall.</h1>"
+echo '
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+* {
+  color: white;
+}
 
+.flex-container {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 100px;
+  width: 60%;
+  margin: 0 auto;
+  background-color: #547792;
+  border-radius: 40px;
+  transition: 0.5s; /* Transition for smooth hover effect */
+}
 
+.button {
+  font-size: 40px;
+  font-weight: 700;
+  transition: 2s;
+}
 
+.flex-container:hover {
+  border-radius: 30px;
+  transform: scale(1.02);  /* Correct syntax for scaling */
+  background-color: #213448;
+  /* The transition is already applied to the container, no need to repeat it */
+}
 
+/* Responsive layout - makes the text smaller for smaller screens */
+@media (max-width: 500px) {
+  .button {
+    font-size: 10px;
+    transition: 2s;
+  }
+}
+</style>
+</head>
+<body>
+<div class="flex-container">
+  <div class="button">Click me</div>
+  
+</div>
+
+</body>
+</html>
+';
 ?>
-
